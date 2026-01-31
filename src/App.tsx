de1e735e14a41296f7812398dd48bd7a8adb3d67
Yassar <ymamoufaka@gmail.com>
2026-01-31 18:47:22 +0100

    Bug lien régler

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import ScriptPage from "./components/ScriptBashPage"; // page pour le script Bash
 import Contact from "./components/Contact";
-import Project from "./components/Project";
 import AdminMessages from "./components/AdminMessage";
 import About from "./components/About";
+import ProjectDetailsWeb from "./components/ProjectDetailWeb";
+import ProjectDetailsIoT from "./components/ProectDetailsIot";
 const App: React.FC = () => {
   return (
     <Router>
@@ -30,8 +31,9 @@ const App: React.FC = () => {
         <Route path="/script" element={<ScriptPage />} />
         <Route path="/project" element={<Projects/>}/>
         <Route path="/admin/message" element={<AdminMessages/>}/>
-        <Route path="/project/:categorie/:name" element={<Project/>}/>
-     
+        <Route path="/project/iot/:name" element={<ProjectDetailsIoT/>}/>
+          <Route path="/project/web/:name" element={<ProjectDetailsWeb />} />
+
       </Routes>
       {/* <Contact/> */}
     </Router>
